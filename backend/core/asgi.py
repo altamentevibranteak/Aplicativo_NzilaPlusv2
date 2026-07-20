f27d02cfcb72ed0c1ea98975dda7a3947a3b19df
altamentevibranteak <andrekumbokuwonza57@gmail.com>
2026-07-20 01:27:53 +0100

    Corrige inicialização do ASGI

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -3,11 +3,13 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 from django.core.asgi import get_asgi_application
+
+# Inicializa o Django PRIMEIRO
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import api.routing
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
